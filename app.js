03624e6f91081597a17a25c971f2ed1a412747aa
chunxu <1150508902@qq.com>
2025-11-08 12:50:51 +0800

    third test commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ App({
         if (!wx.cloud) {
             console.error('请使用 2.2.3 或以上的基础库以使用云能力')
         } else {
-            console.log('wx cloud init startsxxx', wx.cloud);
+            console.log('wx cloud init startsxxxxxss111', wx.cloud);
             this.globalData = {}
 
             wx.cloud.init({
